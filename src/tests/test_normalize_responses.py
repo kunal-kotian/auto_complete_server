947be43ecdb70ef2476c8f7cd6a4a9836ed8a9cf
Kunal Kotian <k.a.kotian@gmail.com>
2018-07-23 22:25:08 -0700

    added tests to check that the normalize_responses module produces the expected signature for responses

---
+++ src/tests/test_normalize_responses.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append("..")   # add the dir one level up to the modules path
+
+import unittest
+import warnings
+
+from normalize_responses import get_signature_to_text_map
+
+
+class testNormalizeResponses(unittest.TestCase):
+    def test_signature_to_text_map(self):
+        """Test that normalization produces expected result."""
+        # suppress warnings to declutter output
+        warnings.simplefilter("ignore")
+        responses = ["We've gotta resolve these 5 issues!"]
+        signature_to_text = get_signature_to_text_map(responses)
+        expected = eval("""{'-PRON- have get to resolve these NUM issue':
+                           ['we have got to resolve these NUM issues']}""")
+        self.assertEqual(signature_to_text, expected)
